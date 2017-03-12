0a95bc06b8c79e73144c207b9ad79edba3c9aa55
davidjhodge <dhodge@genesisappsonline.com>
2017-03-12 18:19:52 -0400

    Basic Navigation Components Added
    Though there is only a camera screen in the app, very navigation infrastructure has been implemented.

---
+++ src/api/products.js
@@ -0,0 +1,12 @@
+
+export function fetchProducts() {
+  // let url = "";
+  // return fetch(url)
+  // .then((response) => response.json())
+  // .then((responseJson) => {
+  //   return responseJson.movies;
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  // });
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Navigator, View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 // This is how you import components from one file to another.
 // The reason it's CameraComponent, not { Camera Component } (Note the brackets)
 // is because in CameraComponent.jsx, export default is used.
@@ -7,12 +7,33 @@ import React, { Component } from 'react';
 // If multiple items were exportable in one file, you would just do "export CameraComponent"
 import CameraComponent from './CameraComponent.js';
 
+// Routes
+const routes = [
+  {title: 'First Scene', index: 0},
+  {title: 'Second Scene', index: 1},
+];
+
 class App extends Component {
   render() {
     return (
-      <CameraComponent />
+      <View style={styles.container}>
+        <Navigator
+          initialRoute={routes[0]}
+          initialRouteStack={routes}
+          renderScene={(route, navigator) =>
+            <CameraComponent />
+          }
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  }
+});
 
 export default App;
