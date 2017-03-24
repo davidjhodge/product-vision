c9d131beba6aeaf06b7e1f11fca8d7a3fc182718
davidjhodge <dhodge@genesisappsonline.com>
2017-03-24 14:32:20 -0400

    Add Routing and Navigation Transitions
    The app now supports nearly all of the routing and animations
    needed for the final product. The login screen now connects to
     the rest of the app. The Camera app contains "Recent" and "Settings"
     buttons that will navigate to modal screens.
    
     Most importantly, when a barcode is successfully detected, a
     new screen is shown. This screen is empty now, but will be used
     to display related products to the user.

--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -20,7 +20,30 @@ class SignUpComponent extends Component {
     this.state = {
 
     };
+  }
 
+  signUp() {
+    // TODO
+    // 1. Add a route to the renderScene method in App.js
+    // 2. Uncomment and call this logic below
+    // this.props.navigator.push({
+    //   name: 'WhateverYourRouteIsCalled',
+    //   type: 'Modal', // Animation type. Could also be 'Normal' or 'None'
+    //   passProps: {
+    //     // If you needed to pass props, they would go here
+    //   }
+    // });
+  }
+
+  navigateToCamera() {
+    // Navigator is passed in as a prop in the App.js component
+    this.props.navigator.push({
+      name: 'Camera',
+      passProps: {
+
+      },
+      type: 'None'
+    });
   }
 
   render() {
@@ -30,14 +53,22 @@ class SignUpComponent extends Component {
           <Text style={styles.mainText}>ProductVision</Text>
           <Text style = {styles.subText}>Find products by scanning barcodes.</Text>
         </View>
-          <TextInput style={styles.loginInput} placeholder = 'Email'></TextInput>
-          <TextInput style={styles.loginInput} placeholder = 'Password'></TextInput>
-          <Button style = {styles.signUpButton} title = "Sign Up"></Button>
+        <TextInput style={styles.loginInput} placeholder = 'Email'></TextInput>
+        <TextInput style={styles.loginInput} placeholder = 'Password'></TextInput>
+        <Button
+          style={styles.signUpButton}
+          title="Sign Up"
+          onPress={this.signUp.bind(this)}>
+        </Button>
+        <Button
+          style={styles.completionButton}
+          title="Complete Login"
+          onPress={this.navigateToCamera.bind(this)}>
+        </Button>
       </View>
 
     );
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -46,7 +77,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#8ec5fc',
-    height: 242
+    // height: 242  //**Setting the explicit height in a flex container is causing problems (I think)
   },
   mainText: {
     color: '#ffffff',
@@ -61,6 +92,13 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   signUpButton:{
+    height: 48,
+    marginRight: 24,
+    marginLeft:24,
+    fontSize: 20,
+    backgroundColor: '#c8c8c8'
+  },
+  completionButton: {
     height: 48,
     marginRight: 24,
     marginLeft:24,
---
+++ src/lib/transitions.js
@@ -0,0 +1,36 @@
+// Transition without animation
+// https://github.com/facebook/react-native/issues/1953#issuecomment-209872368
+
+'use strict';
+
+import React, {
+    Navigator
+} from 'react-native';
+
+const buildStyleInterpolator = require('buildStyleInterpolator');
+
+var NoTransition = {
+    opacity: {
+        from: 1,
+        to: 1,
+        min: 1,
+        max: 1,
+        type: 'linear',
+        extrapolate: false,
+        round: 100
+    }
+};
+
+const Transitions = {
+    NONE: {
+        ...Navigator.SceneConfigs.FadeAndroid,
+        gestures: null,
+        defaultTransitionVelocity: 1000,
+        animationInterpolators: {
+            into: buildStyleInterpolator(NoTransition),
+            out: buildStyleInterpolator(NoTransition)
+        }
+    }
+};
+
+export default Transitions;
